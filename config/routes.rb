607af4c10b90be54cf4e885c0c86f79a676e41ca
Cindy <cindyjiang88@163.com>
2017-01-04 22:21:34 +0800

    add

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-
-  resources :topics
-  root "topics#index"
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+root 'topics#index'
+resources :topics do
+   member do
+     post 'upvote'
+   end
+ end
 end
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
